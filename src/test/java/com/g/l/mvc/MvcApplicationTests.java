2c9c8b9c4342eb091e8b9a8b7c3066ea6c5eb363
fernando-montiel <montiel.fernando.e@gmail.com>
2023-12-11 19:35:30 -0300

    base project

---
+++ src/test/java/com/g/l/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g.l.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
