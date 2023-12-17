60741e57222ea05ebde693d906a7cb6e37e1ecea
EzeFM <montiel.fernando.e@gmail.com>
2023-12-17 11:02:50 -0300

    employee mapper

---
+++ src/main/java/com/g/l/mvc/mapper/EmployeeMapper.java
@@ -0,0 +1,29 @@
+package com.g.l.mvc.mapper;
+
+
+import com.g.l.mvc.dto.request.RequestEmployeeDTO;
+import com.g.l.mvc.model.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring", uses = EmployeeMapper.class)
+@Component
+public interface EmployeeMapper {
+
+
+    @Mappings({
+            @Mapping(source = "dni", target = "dni"),
+            @Mapping(source = "name", target = "name"),
+            @Mapping(source = "cuil", target = "cuil"),
+            @Mapping(source = "salary", target = "salary"),
+            @Mapping(source = "position", target = "position"),
+            @Mapping(source = "phoneNumber", target = "phoneNumber"),
+            @Mapping(source = "direction", target = "direction"),
+            @Mapping(source = "projects", target = "projects")
+    })
+    Employee requestEmployeeDtoToEmployee(RequestEmployeeDTO request);
+
+
+}
